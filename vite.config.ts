ff0d52664c4e03c83b5953f2c032fc40cdc14a9a
Arnob672 <80312770+Arnob90@users.noreply.github.com>
2024-05-04 14:30:55 +0000

    Removed base

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "https://arnob672.github.io/BAFSK-School-Bulletin/"
 })
